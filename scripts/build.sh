78557ebd4118f2407600d572ef7e46b86d615578
alberto56 <albert@mediatribe.net>
2019-10-18 16:01:59 -0400

    Please squash this commit in the pull request

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+DESTINATION_DIR=jekyll/_site
+echo " => Creating our destination directory, $DESTINATION_DIR"
+mkdir -p "$DESTINATION_DIR"
+
+echo " => Building our site"
 docker run --rm \
   --volume="$PWD/jekyll:/srv/jekyll" \
   -it jekyll/jekyll:4 \
