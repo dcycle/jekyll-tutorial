f0b923ab66d218a7c8e9a0759af511e2131e6b04
alberto56 <albert@mediatribe.net>
2019-10-18 15:59:59 -0400

    Please squash this commit in the pull request

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
-docker run --rm --volume="$PWD/jekyll:/srv/jekyll" -it jekyll/jekyll jekyll build
+docker run --rm \
+  --volume="$PWD/jekyll:/srv/jekyll" \
+  -it jekyll/jekyll:4 \
+  jekyll build --trace
