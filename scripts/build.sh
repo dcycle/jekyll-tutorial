35170bf5e85bf35ef42502809506facd74565395
Albert Albala <albert@mediatribe.net>
2019-10-18 16:09:52 -0400

    First passing test on CircleCI (#1)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
 
-docker run --rm --volume="$PWD/jekyll:/srv/jekyll" -it jekyll/jekyll jekyll build
+DESTINATION_DIR=jekyll/_site
+echo " => Creating our destination directory, $DESTINATION_DIR"
+mkdir -p "$DESTINATION_DIR"
+mkdir -p "jekyll/.jekyll-cache"
+
+echo " => Building our site"
+docker run --rm \
+  --volume="$PWD/jekyll:/srv/jekyll":rw \
+  -it jekyll/jekyll:4 \
+  jekyll build --trace
